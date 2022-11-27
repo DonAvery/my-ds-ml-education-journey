05fa7bb12a688cb6d1a930f7006ab1b7b7b34023
Don Avery <don@donavery.com>
2022-11-28 01:23:59 +0300

    Update test.py

--- ml-zoomcamp/Week-9/test.py
+++ ml-zoomcamp/Week-9/test.py
@@ -1,7 +1,7 @@
 import requests
 
 #url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-url = 'https://5hdovghmm6.execute-api.us-east-1.amazonaws.com/test/predict'
+url = 'https://0000000000.execute-api.us-east-1.amazonaws.com/test/predict'
 
 data = {'url': 'http://bit.ly/mlbookcamp-pants'}
 
